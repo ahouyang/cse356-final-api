9c4a75f48da1fe46f474ee4883c3340ee9bb9672
root <root@flask-test.cloud.compas.cs.stonybrook.edu>
2019-04-08 04:31:34 +0000

    front end fixes

--- static/index.js
+++ static/index.js
@@ -1,7 +1,11 @@
-$(()=>{
+$(function(){
+	console.log('hello');
 	$.post('http://130.245.170.86/topten', function(data, textStatus, xhr) {
+		console.log(data);
 		var questions = data.questions;
-		for(var question in questions){
+		for(var i = 0; i < questions.length; i++){
+			var question = questions[i];
+			console.log(question);
 			$('#questions').append('<a href="http://" id="' + question.id + '" class="question"></div>');
 			$('#' + question.id).append('<h2>' + question.title + '</h2><h4>' + question.body + '</h4>');
 		}
