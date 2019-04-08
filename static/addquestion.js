0661473f81b5a2389c8d72fa335cc73c6359a0e6
Andy Ouyang <andy.ouyang@stonybrook.edu>
2019-04-07 22:50:48 -0400

    add question pages

---
+++ static/addquestion.js
@@ -0,0 +1,19 @@
+$('#postquestion').click(function(event) {
+	var request = {};
+	request.title = $('#title').val();
+	request.body = $('#body').val();
+	var tagstring = $('#tags').val();
+	var tags = tagstring.split(' ');
+	request.tags = tags;
+	$.post('http://130.245.170.86/questions/add', $.param(request, true), (data, status, xhr) => {
+			
+			
+			if(data.status == "OK"){
+				$('#submitted').text('Question Posted!');
+				$('#postquestion').attr("disabled", 'true');
+			}
+			else{
+				$('#submitted').text(data.message);
+			}
+		});
+});
